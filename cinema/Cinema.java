381a51c9ed80a0c2b927481836050d181524d0a0
Luke Shea <shealuke@yahoo.com>
2023-02-02 07:38:14 -0600

    Added feature to allow user to pick a seat and show them how much it will cost

--- cinema/Cinema.java
+++ cinema/Cinema.java
@@ -4,37 +4,70 @@ import java.util.*;
 public class Cinema {
     public static void main(String[] args) {
 
-        char[][] seats = new char[7][8];
-        int rowIndex = 1;
-        int rows, seatsPerRow, totalIncome = 0;
+        int rows, seatsPerRow, userRow, userSeatNumber = 0;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter the number of rows:");
         rows = scanner.nextInt();
         System.out.println("Please enter the number of seats in each row:");
         seatsPerRow = scanner.nextInt();
-        System.out.println("Total income:");
-        if (rows * seatsPerRow <= 60) {
-            totalIncome = rows * seatsPerRow * 10;
-            System.out.println("$" + totalIncome);
-        } else if (rows * seatsPerRow > 60) {
-            totalIncome = ((rows / 2) * seatsPerRow) * 10 + ((rows / 2 + rows % 2) * seatsPerRow) * 8;
-            System.out.println("$" + totalIncome);
-        }
+        char[][] seats = new char[rows][seatsPerRow];
 
-        /*
+        // Fill array with S's
         for (int i = 0; i < seats.length; i++) {
             Arrays.fill(seats[i], 'S');
         }
         System.out.println("Cinema:");
-        System.out.println("  1 2 3 4 5 6 7 8");
+        System.out.print(" ");
+        // Print out the ceat column numbers
+        for (int i = 1; i <= seatsPerRow; i++) {
+            System.out.print(" " + i);
+        }
+        System.out.println();
+        // Print out the row numbers and seat indicators
         for (int i = 0; i < seats.length; i++) {
-            System.out.print(rowIndex + " ");
+            System.out.print(i + 1 + " ");
             for (int j = 0; j < seats[i].length; j++) {
                 System.out.print(seats[i][j] + " ");
             }
-            rowIndex++;
             System.out.println();
         }
-        */
+        System.out.println();
+
+        // Let the user pick a seat and tell them the price
+        System.out.println("Enter a row number:");
+        userRow = scanner.nextInt();
+        System.out.println("Enter a seat number in that row:");
+        userSeatNumber = scanner.nextInt();
+        System.out.println();
+        // Calculate and print out the cost for the seat        
+        if (rows * seatsPerRow <= 60) {
+            System.out.println("Ticket price: $10");
+        } else if (rows * seatsPerRow > 60) {
+            if (userRow <= rows / 2) {
+                System.out.println("Ticket price: $10");
+            } else {
+                System.out.println("Ticket price: $8");
+            }
+        }
+        System.out.println();
+        // Set the seat to sold
+        seats[userRow - 1][userSeatNumber - 1] = 'B';
+        // Print out the updated seating arrangement
+        System.out.println("Cinema:");
+        System.out.print(" ");
+        // Print out the ceat column numbers
+        for (int i = 1; i <= seatsPerRow; i++) {
+            System.out.print(" " + i);
+        }
+        System.out.println();
+        // Print out the row numbers and seat indicators
+        for (int i = 0; i < seats.length; i++) {
+            System.out.print(i + 1 + " ");
+            for (int j = 0; j < seats[i].length; j++) {
+                System.out.print(seats[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
     }
 }
