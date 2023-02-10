53f12ddd2fca325b5b0592fe2a561804bcb491ca
Luke Shea <shealuke@yahoo.com>
2023-02-09 22:42:57 -0600

    Initial commit for the Java Swing tutorial

---
+++ javaSwing/Main.java
@@ -0,0 +1,13 @@
+package javaSwing;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // JFrame = a GUI window to add components to
+        
+        MyFrame myframe = new MyFrame();
+    }
+
+}
---
+++ javaSwing/MyFrame.java
@@ -0,0 +1,22 @@
+package javaSwing;
+
+import javax.swing.JFrame;
+import java.awt.*;
+import javax.swing.ImageIcon;
+
+public class MyFrame extends JFrame{
+
+    MyFrame() {
+
+        this.setTitle("JFrame title goes here"); // Sets title of the instance
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Exits out of application rather than hide
+        this.setResizable(false); // Unable to change window size
+        this.setSize(420, 420); // Sets the x dimension and y dismension
+        this.setVisible(true);  // Make frame visible
+
+        ImageIcon image = new ImageIcon("learning_java/javaSwing/money.jpg");
+        this.setIconImage(image.getImage()); // Change icon of this
+        this.getContentPane().setBackground(new Color(0x123456)); // Change color of background
+    }
+    
+}
