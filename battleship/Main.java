d46c6ad794a95f73da8183e8c2d1db99fa24038e
Luke Shea <shealuke@yahoo.com>
2023-02-12 22:20:52 -0600

    Code doesn't break very easily now. Can create gameboards for different players, and you can place ships on them

--- battleship/Main.java
+++ battleship/Main.java
@@ -24,6 +24,7 @@ public class Main {
     }
     public static void printGameBoard(String[][] gameBoard) {
 
+        System.out.println();
         for (int i = 0; i < gameBoard.length; i++) {
             for (int j = 0; j < gameBoard[i].length; j++) {
                 System.out.print(gameBoard[i][j] + " ");
@@ -31,18 +32,44 @@ public class Main {
             System.out.println();
         }
     }
-    public static void placeShipsOnBoard(String[][] gameBoard) {
-        Scanner scanner = new Scanner(System.in);
-        // Aircraft Carrier (5 cells)
-        System.out.println("Enter the coordinates for the Aircraft Carrier (5 cells):");
-        while (true) {
-            String[] coordinates = scanner.nextLine().strip().split(" "); 
+    public static void placeShipOnBoard(String[][] gameBoard, String ship, Scanner scanner) {
 
+        int shipLength = 0;
+        String shipName = "";  
+        switch (ship) {
+            case "Aircraft Carrier":
+                shipName = "Aircraft Carrier";
+                shipLength = 5;
+                break;
+            case "Battleship":
+                shipName = "Battleship";
+                shipLength = 4;
+                break;
+            case "Submarine":
+                shipName = "Submarine";
+                shipLength = 3;
+                break;
+            case "Cruiser":
+                shipName = "Cruiser";
+                shipLength = 3;
+                break;
+            case "Destroyer":
+                shipName = "Destroyer";
+                shipLength = 2;
+                break;
+            default:
+                System.out.println("Invalid entry");
+                break;
+        }
+       
+        System.out.println("\nEnter the coordinates for the " + shipName + " (" + shipLength + " cells):\n");
+        while (true) {
+            String[] coordinates = scanner.nextLine().strip().toUpperCase().split(" "); 
             // Check for either vertical or horizontal placement
             if (coordinates[0].charAt(0) == coordinates[1].charAt(0)) { // Horizontal
                 // Check for the correct length of the piece
-                if (Math.abs(Integer.parseInt(coordinates[0].substring(1)) - Integer.parseInt(coordinates[1].substring(1))) != 4) {
-                    System.out.println("Error! Wrong length of the Aircraft Carrier! Try Again: \n");
+                if (Math.abs(Integer.parseInt(coordinates[0].substring(1)) - Integer.parseInt(coordinates[1].substring(1))) != shipLength - 1) {
+                    System.out.println("\nError! Wrong length of the " + shipName + "! Try Again: \n");
                 } else {
                     // Place the aircraft carrier on the board
                     // Find the row we need to be on
@@ -56,7 +83,7 @@ public class Main {
                     }
                     // Check for failure
                     if (indexRow == -1) {
-                        System.out.println("Error! Those coordinates do not exist! Try Again: \n");
+                        System.out.println("\nError! Wrong ship location! Try again:\n");
                     } else {
                         int shipStartIndex = Integer.parseInt(coordinates[0].substring(1));
                         int shipEndIndex = Integer.parseInt(coordinates[1].substring(1));
@@ -66,20 +93,32 @@ public class Main {
                             shipStartIndex = shipEndIndex;
                             shipEndIndex = temp;
                         }
+                        // Check to make sure the start and end index are on the board
+                        if (shipStartIndex < 1 || shipEndIndex > 10) {
+                            System.out.println("\nError! Wrong ship location! Try again:\n");
+                            continue;
+                        }
                         // Check to make sure we aren't trying place a ship too close to another one
-                        for (int i = shipStartIndex; i < shipEndIndex; i++) {
+                        boolean tooClose = false;
+                        for (int i = shipStartIndex; i <= shipEndIndex; i++) {
                             // For the very first element, we need to check to the left
                             if (i == shipStartIndex) {
                                 if (gameBoard[indexRow][i - 1].equals("O")) {
-                                    System.out.println("Error! You placed it too close to another one. Try again:\n");
-                                    continue;
+                                    tooClose = true;
+                                    System.out.println("\nError! You placed it too close to another one. Try again:\n");
+                                    break;
                                 }
                             }
                             // If not on the first element, then check above, below, and right, in that order
-                            if (gameBoard[indexRow - 1][i].equals("O") || gameBoard[indexRow + 1][i].equals("O") || gameBoard[indexRow][i + 1].equals("O")) {
-                                System.out.println("Error! You placed it too close to another one. Try again:\n");
-                                continue;
+                            // Be careful when checking to the right or below as this can go out of bounds
+                            if (gameBoard[indexRow - 1][i].equals("O") || (indexRow < 10 && gameBoard[indexRow + 1][i].equals("O")) || (i < 10 && gameBoard[indexRow][i + 1].equals("O"))) {
+                                tooClose = true;
+                                System.out.println("\nError! You placed it too close to another one. Try again:\n");
+                                break;
                             } 
+                        }
+                        if (tooClose) {
+                            continue;
                         }
                         // If we get to here then we should be able to place the piece on the board without issue
                         for (int i = shipStartIndex; i <= shipEndIndex; i++) {
@@ -91,8 +130,8 @@ public class Main {
             } else if (coordinates[0].charAt(1) == coordinates[1].charAt(1)) { // Vertical
                 // Check for the correct length of the piece
 
-                if (Math.abs((int)coordinates[0].charAt(0) - (int)coordinates[1].charAt(0)) != 4) {
-                    System.out.println("Error! Wrong length of the Aircraft Carrier! Try Again: \n");
+                if (Math.abs((int)coordinates[0].charAt(0) - (int)coordinates[1].charAt(0)) != shipLength - 1) {
+                    System.out.println("\nError! Wrong length of the " + shipName + "! Try Again: \n");
                 } else {
                     // Place the aircraft carrier on the board
                     // Find the column we need to be in
@@ -106,7 +145,7 @@ public class Main {
                     }
                     // Check for failure
                     if (indexCol == -1) {
-                        System.out.println("Error! Those coordinates do not exist! Try Again: \n");
+                        System.out.println("\nError! Wrong ship location! Try again:\n");
                     } else {
                         int shipStartIndex = (int)coordinates[0].charAt(0) - 64;
                         int shipEndIndex = (int)coordinates[1].charAt(0) - 64;
@@ -116,20 +155,33 @@ public class Main {
                             shipStartIndex = shipEndIndex;
                             shipEndIndex = temp;
                         }
+                        // Check to make sure the start and end index are on the board
+                        if (shipStartIndex < 1 || shipEndIndex > 10) {
+                            System.out.println("\nError! Wrong ship location! Try again:\n");
+                            continue;
+                        }
                         // Check to make sure we aren't trying place a ship too close to another one
-                        for (int i = shipStartIndex; i < shipEndIndex; i++) {
+                        boolean tooClose = false;
+                        for (int i = shipStartIndex; i <= shipEndIndex; i++) {
                             // For the very first element, we need to check above
                             if (i == shipStartIndex) {
                                 if (gameBoard[i - 1][indexCol].equals("O")) {
-                                    System.out.println("Error! You placed it too close to another one. Try again:\n");
-                                    continue;
+                                    tooClose = true;
+                                    System.out.println("\nError! You placed it too close to another one. Try again:\n");
+                                    break;
                                 }
                             }
                             // If not on the first element, then check left, right, and below, in that order
-                            if (gameBoard[i][indexCol - 1].equals("O") || gameBoard[i][indexCol + 1].equals("O") || gameBoard[i + 1][indexCol].equals("O")) {
-                                System.out.println("Error! You placed it too close to another one. Try again:\n");
-                                continue;
+                            // Be careful with looking right and below as this can go out of bounds
+                            if (gameBoard[i][indexCol - 1].equals("O") || (indexCol < 10 && gameBoard[i][indexCol + 1].equals("O")) || (i < 10 && gameBoard[i + 1][indexCol].equals("O"))) {
+                                tooClose = true;
+                                System.out.println("\nError! You placed it too close to another one. Try again:\n");
+                                break;
                             } 
+                        }
+                        if (tooClose) {
+
+                            continue;
                         }
                         // If we get to here then we should be able to place the piece on the board without issue
                         for (int i = shipStartIndex; i <= shipEndIndex; i++) {
@@ -142,26 +194,31 @@ public class Main {
             
             } else { // Diagnonal placement
 
-                System.out.println("Error! Pieces cannot be placed diagonally! Try again:\n");
+                System.out.println("\nError! Wrong ship location! Try again:\n");
                 continue;
             }
         }
-        // Battleship (4 cells)
-
-        // Submarine (3 cells)
-
-        // Cruiser (3 cells)
-
-        // Destroyer (2 cells)
     }
+    
+
     public static void main(String[] args) {
 
         String[][] player1 = createGameBoard();
-        String[][] player2 = createGameBoard();
-
-        placeShipsOnBoard(player1);
+        //String[][] player2 = createGameBoard();
+        Scanner scanner = new Scanner(System.in);
         printGameBoard(player1);
-        //placeShipsOnBoard(player2);
+        placeShipOnBoard(player1, "Aircraft Carrier", scanner);
+        printGameBoard(player1);
+        placeShipOnBoard(player1, "Battleship", scanner);
+        printGameBoard(player1);
+        placeShipOnBoard(player1, "Submarine", scanner);
+        printGameBoard(player1);
+        placeShipOnBoard(player1, "Cruiser", scanner);
+        printGameBoard(player1);
+        placeShipOnBoard(player1, "Destroyer", scanner);
+        printGameBoard(player1);
+
+        scanner.close();
         
 
     }
