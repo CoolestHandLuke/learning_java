b7c53b416317d9a30eb2180ef64a70075ea72456
Luke Shea <shealuke@yahoo.com>
2023-02-23 23:58:43 -0600

    Added in the regex to count the number of words and sentences in the input

--- readability/Main.java
+++ readability/Main.java
@@ -7,13 +7,28 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int textInput = scanner.nextLine().length();
-
-        if (textInput > 100) {
+        String textInput = scanner.nextLine();
+        String[] splitSentence = textInput.split(" ");
+        String sentenceFinder = ".+[.!?]";
+        double wordCount = splitSentence.length;
+        double sentenceCount = 0; 
+        for (int i = 0; i < splitSentence.length; i++) {
+            if (splitSentence[i].matches(sentenceFinder)) {
+                sentenceCount++;
+            } else if (i == splitSentence.length - 1) {
+                sentenceCount++;
+            }
+        }
+        double averageWordsPerSentece = Math.ceil(wordCount / sentenceCount);
+        System.out.println(wordCount);
+        System.out.println(sentenceCount);
+        
+        if (averageWordsPerSentece > 10) {
             System.out.println("HARD");
         } else {
             System.out.println("EASY");
         }
+        
         scanner.close();
      }
 }
