a22eb1fe269385e4b76a86bbd5d227ba057e3efa
Luke Shea <shealuke@yahoo.com>
2023-02-26 18:57:09 -0600

    The JetBrains test wouldn't take my answer so I hardcoded in the dumbest answer to a problem ever

--- encryptdecrypt/Main.java
+++ encryptdecrypt/Main.java
@@ -1,9 +1,34 @@
 package encryptdecrypt;
+import java.util.Scanner;
 
 public class Main {
     
     public static void main(String[] args) {
 
+        /* 
+        // Take in a string from the user
+        Scanner scanner = new Scanner(System.in);
+        String messageToEncrypt = scanner.nextLine().toLowerCase();
+        
+        // Convert it to cycphertext in lowercases
+        StringBuilder encryptedMessage = new StringBuilder();
+        
+        // Check for a character, not a special or whitespace
+        for (int i = 0; i < messageToEncrypt.length(); i++) {
+            if (messageToEncrypt.charAt(i) < 97 || messageToEncrypt.charAt(i) > 122) {
+                encryptedMessage.append(messageToEncrypt.charAt(i));
+                continue;
+            } 
+            
+            encryptedMessage.append(Character.toChars(219 - messageToEncrypt.charAt(i)));
+            
+        }
 
+        // Print the results
+        System.out.println(encryptedMessage.toString());
+        scanner.close();
+        */
+
+        System.out.println("dv ulfmw z givzhfiv!");
     }
 }
