23d2f0d5b81939f8e5104505f82abe42df812324
Luke Shea <shealuke@yahoo.com>
2023-02-26 19:32:04 -0600

    Added the ability to select whether to encode or decode a message. It will now encode/decode all characters/whitespaces/special characters, regardless of case

--- encryptdecrypt/Main.java
+++ encryptdecrypt/Main.java
@@ -5,39 +5,61 @@ public class Main {
     
     public static void main(String[] args) {
 
-        
-        // Take in a string and int key from the user
+        // read a target operation, then a string, then a key
+        // Make a decision based on the inputs and print out the result
+
         Scanner scanner = new Scanner(System.in);
-        String messageToEncrypt = scanner.nextLine().toLowerCase();
-        int key = scanner.nextInt();
-        scanner.nextLine(); // eat the \n from this input
-        
-        StringBuilder encryptedMessage = new StringBuilder();
-        
-        // Check for a character, not a special or whitespace
-        for (int i = 0; i < messageToEncrypt.length(); i++) {
-            if (messageToEncrypt.charAt(i) < 97 || messageToEncrypt.charAt(i) > 122) {
-                encryptedMessage.append(messageToEncrypt.charAt(i));
-                continue;
-            } 
+        String flag = scanner.nextLine();
+        int key = 0;
 
-            // Convert it to cyphertext in lowercases based on the input key
-            // Need to loop to make sure we go back to the beginning of the alphabet if we exceed 122
-            
-            int temp = Character.valueOf(messageToEncrypt.charAt(i)) + key;
-            while (temp > 122) {
-                temp = temp - 26;
-            }          
-            encryptedMessage.append(Character.toChars(temp));
+        switch(flag) {
+            case "enc":
+                String messageToEncrypt = scanner.nextLine();
+                key = scanner.nextInt();
+                scanner.nextLine(); // eat the \n from the previous input
+                enc(messageToEncrypt, key);
+                break;
+            case "dec":
+                String messageToDecrypt = scanner.nextLine();
+                key = scanner.nextInt();
+                scanner.nextLine(); // eat the \n from the previous input
+                dec(messageToDecrypt, key);
+                break;
+            default:
+                System.out.println("Invalid input");
 
-            }
-            
+        }
+
         
-
-        // Print the results
-        System.out.println(encryptedMessage.toString());
+        
         scanner.close();
         
+
+    }
+
+    public static void enc(String messageToEncrypt, int key) {
+
+        // Encrypt everything now, not just characters, so don't need to check anymore
+        StringBuilder encryptedMessage = new StringBuilder();
+        for (int i = 0; i < messageToEncrypt.length(); i++) {
+            // Convert it to cyphertext in lowercases based on the input key
+            encryptedMessage.append(Character.toChars(Character.valueOf(messageToEncrypt.charAt(i)) + key));
+        }
+        // Print the results
+        System.out.println(encryptedMessage.toString());
+
+    }
+
+    public static void dec(String messageToDecrypt, int key) {
+
+        // Same as the encrypt function, but in reverse
+        StringBuilder decryptedMessage = new StringBuilder();
+        for (int i = 0; i < messageToDecrypt.length(); i++) {
+            // Convert it to cyphertext in lowercases based on the input key
+            decryptedMessage.append(Character.toChars(Character.valueOf(messageToDecrypt.charAt(i)) - key));
+        }
+        // Print the results
+        System.out.println(decryptedMessage.toString());
 
     }
 }
