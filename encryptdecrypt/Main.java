c49be3f967c2c5cf1cb9ca28cfad6c3a4744f9b2
Luke Shea <shealuke@yahoo.com>
2023-02-26 19:09:34 -0600

    Added in the ability to ACTUALLY TAKE IN SOME USER INPUT YEAHHHH BUDDYgit add .git add . Also you can enter a key that shifts over all the characters by that number. Wraps around the end of the alphabet if reached

--- encryptdecrypt/Main.java
+++ encryptdecrypt/Main.java
@@ -5,12 +5,13 @@ public class Main {
     
     public static void main(String[] args) {
 
-        /* 
-        // Take in a string from the user
+        
+        // Take in a string and int key from the user
         Scanner scanner = new Scanner(System.in);
         String messageToEncrypt = scanner.nextLine().toLowerCase();
+        int key = scanner.nextInt();
+        scanner.nextLine(); // eat the \n from this input
         
-        // Convert it to cycphertext in lowercases
         StringBuilder encryptedMessage = new StringBuilder();
         
         // Check for a character, not a special or whitespace
@@ -19,16 +20,24 @@ public class Main {
                 encryptedMessage.append(messageToEncrypt.charAt(i));
                 continue;
             } 
+
+            // Convert it to cyphertext in lowercases based on the input key
+            // Need to loop to make sure we go back to the beginning of the alphabet if we exceed 122
             
-            encryptedMessage.append(Character.toChars(219 - messageToEncrypt.charAt(i)));
+            int temp = Character.valueOf(messageToEncrypt.charAt(i)) + key;
+            while (temp > 122) {
+                temp = temp - 26;
+            }          
+            encryptedMessage.append(Character.toChars(temp));
+
+            }
             
-        }
+        
 
         // Print the results
         System.out.println(encryptedMessage.toString());
         scanner.close();
-        */
+        
 
-        System.out.println("dv ulfmw z givzhfiv!");
     }
 }
